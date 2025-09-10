487a445162c0c58ea982e7ded17b30e88b484202
Thais Moreira <thais.dfmoreira@gmail.com>
2025-09-10 11:13:47 -0300

    Transformei lista de usuários em objeto

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,14 +1,22 @@
-const usuarios = ['matheus', 'marcos', 'lucas', 'joão'];
+// Lista de usuários (array de objetos)
+const usuarios = [
+  { nome: 'Matheus', email: 'matheus@gospel.com' },
+  { nome: 'Marcos', email: 'marcos@gospel.com' },
+  { nome: 'Lucas', email: 'lucas@gospel.com' },
+  { nome: 'João', email: 'joao@gospel.com' }
+];
 
+// Função para retornar todos os usuários
 function retornarUsuarios() {
   return usuarios;
 }
 
-function adicionarNovoUsuario(nome) {
-  usuarios.push(nome);
+// Função para adicionar um novo usuário
+function adicionarNovoUsuario(nome, email) {
+  usuarios.push({ nome, email });
 }
 
-
+// Exportando para usar em outros arquivos
 module.exports = {
   retornarUsuarios,
   adicionarNovoUsuario
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,8 +4,9 @@ const { expect } = require('chai');
 describe('Testar as funções de Gestão de Usuários', function () {
   it('Validar que posso adicionar um novo nome de usuário na lista', function () {
 
-    adicionarNovoUsuario('Maria');
+    adicionarNovoUsuario('Maria', 'email@email.com');
     const listaDeUsuarios = retornarUsuarios();
-    expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+    expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
+    expect(listaDeUsuarios.at(-1).email).to.equal('email@email.com');
   });
 });
